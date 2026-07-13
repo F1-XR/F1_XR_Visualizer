af5824827dd6f36dc77054c850bcff4207fde240
YunSangHwa <dbstkdghk8@gmail.com>
2026-07-13 18:34:00 +0900

    2차 수정 확인 완

--- Assets/F1_XR_Visualizer/02_Scripts/RestAPI/Replay/Car/ReplayCarView.cs
+++ Assets/F1_XR_Visualizer/02_Scripts/RestAPI/Replay/Car/ReplayCarView.cs
@@ -1,5 +1,6 @@
 using System.Collections.Generic;
 using UnityEngine;
+using static F1XR.RestAPI.Replay.ReplayCarVisualUtil;
 
 namespace F1XR.RestAPI.Replay
 {
@@ -37,11 +38,6 @@ namespace F1XR.RestAPI.Replay
         private static readonly int BaseColorId = Shader.PropertyToID("_BaseColor");
         private static readonly int ColorId = Shader.PropertyToID("_Color");
         private static readonly int EmissionColorId = Shader.PropertyToID("_EmissionColor");
-        private static readonly int SurfaceId = Shader.PropertyToID("_Surface");
-        private static readonly int SrcBlendId = Shader.PropertyToID("_SrcBlend");
-        private static readonly int DstBlendId = Shader.PropertyToID("_DstBlend");
-        private static readonly int ZWriteId = Shader.PropertyToID("_ZWrite");
-        private static readonly int CullId = Shader.PropertyToID("_Cull");
         private TextMesh label;
         private LineRenderer labelLine;
         private MeshRenderer labelBackground;
@@ -268,92 +264,6 @@ namespace F1XR.RestAPI.Replay
             return renderer;
         }
 
-        private static Material CreateUnlitMaterial(Color color)
-        {
-            Shader shader = Shader.Find("Sprites/Default");
-            if (shader == null)
-                shader = Shader.Find("Universal Render Pipeline/Unlit");
-
-            Material material = new Material(shader);
-            material.color = color;
-
-            if (material.HasProperty("_BaseColor"))
-                material.SetColor("_BaseColor", color);
-
-            if (material.HasProperty("_Color"))
-                material.SetColor("_Color", color);
-
-            if (material.HasProperty("_Surface"))
-                material.SetFloat("_Surface", 1f);
-
-            if (material.HasProperty("_SrcBlend"))
-                material.SetFloat("_SrcBlend", (float)UnityEngine.Rendering.BlendMode.SrcAlpha);
-
-            if (material.HasProperty("_DstBlend"))
-                material.SetFloat("_DstBlend", (float)UnityEngine.Rendering.BlendMode.OneMinusSrcAlpha);
-
-            if (material.HasProperty("_ZWrite"))
-                material.SetFloat("_ZWrite", 0f);
-
-            if (material.HasProperty("_Metallic"))
-                material.SetFloat("_Metallic", 0f);
-
-            if (material.HasProperty("_Smoothness"))
-                material.SetFloat("_Smoothness", 0f);
-
-            if (material.HasProperty("_Glossiness"))
-                material.SetFloat("_Glossiness", 0f);
-
-            material.EnableKeyword("_SURFACE_TYPE_TRANSPARENT");
-            material.renderQueue = 3000;
-            return material;
-        }
-
-        private static Material CreateSelectionMaterial(Color color)
-        {
-            Shader shader = Shader.Find("Universal Render Pipeline/Unlit");
-            if (shader == null)
-                shader = Shader.Find("Sprites/Default");
-
-            Material material = new Material(shader);
-            material.name = "Runtime_SelectedCarFx";
-            SetMaterialColor(material, color);
-
-            if (material.HasProperty(SurfaceId))
-                material.SetFloat(SurfaceId, 1f);
-
-            if (material.HasProperty(SrcBlendId))
-                material.SetFloat(SrcBlendId, (float)UnityEngine.Rendering.BlendMode.SrcAlpha);
-
-            if (material.HasProperty(DstBlendId))
-                material.SetFloat(DstBlendId, (float)UnityEngine.Rendering.BlendMode.One);
-
-            if (material.HasProperty(ZWriteId))
-                material.SetFloat(ZWriteId, 0f);
-
-            if (material.HasProperty(CullId))
-                material.SetFloat(CullId, (float)UnityEngine.Rendering.CullMode.Off);
-
-            material.EnableKeyword("_SURFACE_TYPE_TRANSPARENT");
-            material.renderQueue = 3100;
-            return material;
-        }
-
-        private static Material CreateTextMaterial(TextMesh text, Color color)
-        {
-            Shader shader = Shader.Find("GUI/Text Shader");
-            if (shader == null)
-                shader = Shader.Find("Sprites/Default");
-
-            Material material = new Material(shader);
-            if (text.font != null && text.font.material != null)
-                material.mainTexture = text.font.material.mainTexture;
-
-            SetMaterialColor(material, color);
-            material.renderQueue = 3000;
-            return material;
-        }
-
         private void OnDestroy()
         {
             if (labelTextMaterial != null)
@@ -430,7 +340,7 @@ namespace F1XR.RestAPI.Replay
                 selectionRing.shadowCastingMode = UnityEngine.Rendering.ShadowCastingMode.Off;
                 selectionRing.receiveShadows = false;
                 selectionRing.material = selectionRingMaterial;
-                selectionRingMesh = CreateRingMesh("SelectedCarGroundRing", out selectionRingVertices);
+                selectionRingMesh = CreateRingMesh("SelectedCarGroundRing", SelectionRingSegments, out selectionRingVertices);
                 ringFilter.sharedMesh = selectionRingMesh;
                 created = true;
             }
@@ -444,7 +354,7 @@ namespace F1XR.RestAPI.Replay
                 selectionPulse.shadowCastingMode = UnityEngine.Rendering.ShadowCastingMode.Off;
                 selectionPulse.receiveShadows = false;
                 selectionPulse.material = selectionPulseMaterial;
-                selectionPulseMesh = CreateRingMesh("SelectedCarPulse", out selectionPulseVertices);
+                selectionPulseMesh = CreateRingMesh("SelectedCarPulse", SelectionRingSegments, out selectionPulseVertices);
                 pulseFilter.sharedMesh = selectionPulseMesh;
                 pulse.SetActive(false);
                 created = true;
@@ -478,7 +388,7 @@ namespace F1XR.RestAPI.Replay
                 leaderRing.shadowCastingMode = UnityEngine.Rendering.ShadowCastingMode.Off;
                 leaderRing.receiveShadows = false;
                 leaderRing.material = leaderRingMaterial;
-                leaderRingMesh = CreateRingMesh("RaceLeaderGroundRing", out leaderRingVertices);
+                leaderRingMesh = CreateRingMesh("RaceLeaderGroundRing", SelectionRingSegments, out leaderRingVertices);
                 ringFilter.sharedMesh = leaderRingMesh;
                 created = true;
             }
@@ -507,8 +417,10 @@ namespace F1XR.RestAPI.Replay
 
             SetMaterialColor(leaderRingMaterial, WithAlpha(LeaderFxColor, alpha));
             UpdateRingMesh(
+                transform,
                 leaderRingMesh,
                 leaderRingVertices,
+                SelectionRingSegments,
                 localCenter,
                 radius,
                 LeaderRingInnerRatio,
@@ -547,8 +459,10 @@ namespace F1XR.RestAPI.Replay
             Vector3 localCenter = transform.InverseTransformPoint(worldCenter);
 
             UpdateRingMesh(
+                transform,
                 selectionRingMesh,
                 selectionRingVertices,
+                SelectionRingSegments,
                 localCenter,
                 radius,
                 SelectionRingInnerRatio,
@@ -577,7 +491,15 @@ namespace F1XR.RestAPI.Replay
             Color color = CurrentSelectionFxColor();
             SetMaterialColor(selectionPulseMaterial, WithAlpha(color, pulseAlpha));
             Vector3 pulseCenter = localCenter + Vector3.up * LocalDistance(Mathf.Max(radius * SelectionPulseHeightRatio, 0.0008f));
-            UpdateRingMesh(selectionPulseMesh, selectionPulseVertices, pulseCenter, pulseRadius, SelectionPulseInnerRatio, -selectionAge * SelectionRingRotationSpeed * 0.65f);
+            UpdateRingMesh(
+                transform,
+                selectionPulseMesh,
+                selectionPulseVertices,
+                SelectionRingSegments,
+                pulseCenter,
+                pulseRadius,
+                SelectionPulseInnerRatio,
+                -selectionAge * SelectionRingRotationSpeed * 0.65f);
             selectionPulseAge += Time.deltaTime;
         }
 
@@ -662,77 +584,6 @@ namespace F1XR.RestAPI.Replay
                 IsLeaderEffectRenderer(renderer);
         }
 
-        private static Mesh CreateRingMesh(string meshName, out Vector3[] vertices)
-        {
-            Mesh mesh = new Mesh { name = meshName };
-            vertices = new Vector3[SelectionRingSegments * 2];
-            Vector2[] uvs = new Vector2[vertices.Length];
-            Color[] colors = new Color[vertices.Length];
-            int[] triangles = new int[SelectionRingSegments * 6];
-
-            for (int i = 0; i < SelectionRingSegments; i++)
-            {
-                float angle = i / (float)SelectionRingSegments * Mathf.PI * 2f;
-                float cos = Mathf.Cos(angle);
-                float sin = Mathf.Sin(angle);
-                int inner = i * 2;
-                int outer = inner + 1;
-
-                vertices[inner] = new Vector3(cos * 0.72f, 0f, sin * 0.72f);
-                vertices[outer] = new Vector3(cos, 0f, sin);
-                uvs[inner] = new Vector2(0f, i / (float)SelectionRingSegments);
-                uvs[outer] = new Vector2(1f, i / (float)SelectionRingSegments);
-                colors[inner] = new Color(1f, 1f, 1f, 0.24f);
-                colors[outer] = Color.white;
-
-                int nextInner = (i + 1) % SelectionRingSegments * 2;
-                int nextOuter = nextInner + 1;
-                int triangle = i * 6;
-                triangles[triangle] = inner;
-                triangles[triangle + 1] = outer;
-                triangles[triangle + 2] = nextOuter;
-                triangles[triangle + 3] = inner;
-                triangles[triangle + 4] = nextOuter;
-                triangles[triangle + 5] = nextInner;
-            }
-
-            mesh.vertices = vertices;
-            mesh.uv = uvs;
-            mesh.colors = colors;
-            mesh.triangles = triangles;
-            mesh.RecalculateNormals();
-            mesh.RecalculateBounds();
-            return mesh;
-        }
-
-        private void UpdateRingMesh(Mesh mesh, Vector3[] vertices, Vector3 localCenter, float worldOuterRadius, float innerRatio, float yawDegrees)
-        {
-            if (mesh == null || vertices == null)
-                return;
-
-            Vector3 scale = transform.lossyScale;
-            float outerX = worldOuterRadius / Mathf.Max(0.0001f, Mathf.Abs(scale.x));
-            float outerZ = worldOuterRadius / Mathf.Max(0.0001f, Mathf.Abs(scale.z));
-            float innerX = outerX * innerRatio;
-            float innerZ = outerZ * innerRatio;
-            float yaw = yawDegrees * Mathf.Deg2Rad;
-
-            for (int i = 0; i < SelectionRingSegments; i++)
-            {
-                float angle = i / (float)SelectionRingSegments * Mathf.PI * 2f + yaw;
-                float cos = Mathf.Cos(angle);
-                float sin = Mathf.Sin(angle);
-                int inner = i * 2;
-                int outer = inner + 1;
-
-                vertices[inner] = localCenter + new Vector3(cos * innerX, 0f, sin * innerZ);
-                vertices[outer] = localCenter + new Vector3(cos * outerX, 0f, sin * outerZ);
-            }
-
-            mesh.vertices = vertices;
-            mesh.RecalculateBounds();
-        }
-
         private void UpdateLabelLayout()
         {
             if (!TryGetCarBounds(out Bounds bounds))
@@ -802,34 +653,6 @@ namespace F1XR.RestAPI.Replay
             labelBackground.transform.localScale = new Vector3(labelWidth, labelHeight, 1f);
         }
 
-        private static Vector3 ToLocalScale(Transform target, float worldWidth, float worldHeight)
-        {
-            Transform parent = target.parent;
-            if (parent == null)
-                return new Vector3(worldWidth, worldHeight, 1f);
-
-            Vector3 scale = parent.lossyScale;
-            return new Vector3(
-                worldWidth / Mathf.Max(0.0001f, Mathf.Abs(scale.x)),
-                worldHeight / Mathf.Max(0.0001f, Mathf.Abs(scale.y)),
-                1f
-            );
-        }
-
-        private static Vector3 ToLocalScale(Transform target, float worldWidth, float worldHeight, float worldDepth)
-        {
-            Transform parent = target.parent;
-            if (parent == null)
-                return new Vector3(worldWidth, worldHeight, worldDepth);
-
-            Vector3 scale = parent.lossyScale;
-            return new Vector3(
-                worldWidth / Mathf.Max(0.0001f, Mathf.Abs(scale.x)),
-                worldHeight / Mathf.Max(0.0001f, Mathf.Abs(scale.y)),
-                worldDepth / Mathf.Max(0.0001f, Mathf.Abs(scale.z))
-            );
-        }
-
         private MeshRenderer CreateLabelDot(string objectName)
         {
             GameObject obj = GameObject.CreatePrimitive(PrimitiveType.Sphere);
@@ -858,44 +681,10 @@ namespace F1XR.RestAPI.Replay
             dot.transform.localScale = ToLocalScale(dot.transform, worldSize);
         }
 
-        private static Vector3 ToLocalScale(Transform target, float worldSize)
-        {
-            Transform parent = target.parent;
-            if (parent == null)
-                return Vector3.one * worldSize;
-
-            Vector3 scale = parent.lossyScale;
-            return new Vector3(
-                worldSize / Mathf.Max(0.0001f, Mathf.Abs(scale.x)),
-                worldSize / Mathf.Max(0.0001f, Mathf.Abs(scale.y)),
-                worldSize / Mathf.Max(0.0001f, Mathf.Abs(scale.z))
-            );
-        }
-
-        private static void SetMaterialColor(Material material, Color color)
-        {
-            if (material == null)
-                return;
-
-            material.color = color;
-
-            if (material.HasProperty("_BaseColor"))
-                material.SetColor("_BaseColor", color);
-
-            if (material.HasProperty("_Color"))
-                material.SetColor("_Color", color);
-        }
-
         private static void AddRenderer(List<Renderer> renderers, Renderer renderer)
         {
             if (renderer != null)
                 renderers.Add(renderer);
-        }
-
-        private static Color WithAlpha(Color color, float alpha)
-        {
-            color.a = alpha;
-            return color;
         }
 
         private void SetLabelObjectsActive(bool active)
