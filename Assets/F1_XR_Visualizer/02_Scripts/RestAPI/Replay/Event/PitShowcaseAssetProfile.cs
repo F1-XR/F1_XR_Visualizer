fbdb4dbb03933a453ae3e1df2a10e38c108c569d
YunSangHwa <dbstkdghk8@gmail.com>
2026-08-24 15:42:19 +0900

    피트 스탑 초안: 위치, 방향, 애니메이션 어색

--- Assets/F1_XR_Visualizer/02_Scripts/RestAPI/Replay/Event/PitShowcaseAssetProfile.cs
+++ Assets/F1_XR_Visualizer/02_Scripts/RestAPI/Replay/Event/PitShowcaseAssetProfile.cs
@@ -16,6 +16,16 @@ namespace F1XR.RestAPI.Replay
         [SerializeField] private GameObject tyreVisualPrefab;
         [SerializeField] private TMP_FontAsset displayFont;
 
+        [Header("First Milestone Choreography")]
+        [SerializeField] private GameObject pitCrewPrefab;
+        [SerializeField] private RuntimeAnimatorController choreographyBaseController;
+        [SerializeField] private AnimationClip wheelGunnerFull;
+        [SerializeField] private AnimationClip wheelOffFullL;
+        [SerializeField] private AnimationClip wheelOnFullL;
+        [SerializeField] private AnimationClip frontJackFullL;
+        [SerializeField] private AnimationClip rearJackFullR;
+        [SerializeField] private AnimationClip pitSignalFullR;
+
         public GameObject WheelGunPrefab => wheelGunPrefab;
         public AudioClip WheelGunLoopClip => wheelGunLoopClip;
         public AudioClip SuspensionSettleClip => suspensionSettleClip;
@@ -23,5 +33,25 @@ namespace F1XR.RestAPI.Replay
         public AudioClip LaunchClip => launchClip;
         public GameObject TyreVisualPrefab => tyreVisualPrefab;
         public TMP_FontAsset DisplayFont => displayFont;
+        public GameObject PitCrewPrefab => pitCrewPrefab;
+        public RuntimeAnimatorController ChoreographyBaseController =>
+            choreographyBaseController;
+        public AnimationClip WheelGunnerFull => wheelGunnerFull;
+        public AnimationClip WheelOffFullL => wheelOffFullL;
+        public AnimationClip WheelOnFullL => wheelOnFullL;
+        public AnimationClip FrontJackFullL => frontJackFullL;
+        public AnimationClip RearJackFullR => rearJackFullR;
+        public AnimationClip PitSignalFullR => pitSignalFullR;
+
+        public bool HasFirstMilestoneChoreographyAssets =>
+            pitCrewPrefab != null &&
+            choreographyBaseController != null &&
+            wheelGunPrefab != null &&
+            wheelGunnerFull != null &&
+            wheelOffFullL != null &&
+            wheelOnFullL != null &&
+            frontJackFullL != null &&
+            rearJackFullR != null &&
+            pitSignalFullR != null;
     }
 }
