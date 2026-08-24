cd3711ee4f55759175e0c020e7157116e921dd39
harryen88 <harryen8448@gmail.com>
2026-08-24 21:53:18 +0900

    Feat: 비구름 구현 1차

--- Assets/F1Weather/Cloud/Scripts/RainArea.cs
+++ Assets/F1Weather/Cloud/Scripts/RainArea.cs
@@ -4,39 +4,49 @@ public class RainArea : MonoBehaviour
 {
     [Header("Particle Systems")]
     public ParticleSystem rainMain;
+    public ParticleSystem rainFine;
     public ParticleSystem rainSplash;
     public ParticleSystem rainRipple;
 
     [Header("Intensity Mapping")]
     [Tooltip("Emission rate at full intensity")]
     public float maxEmissionRate = 80f;
+    [Tooltip("Fine-drizzle emission rate at full intensity")]
+    public float maxFineEmissionRate = 160f;
 
     ParticleSystem.EmissionModule _emMain;
+    ParticleSystem.EmissionModule _emFine;
     bool _initialized;
 
-    void Awake()
+    void Awake() => Init();
+
+    void Init()
     {
-        if (rainMain)
-        {
-            _emMain = rainMain.emission;
-            _initialized = true;
-        }
+        if (_initialized || rainMain == null) return;
+        _emMain = rainMain.emission;
+        if (rainFine) _emFine = rainFine.emission;
+        _initialized = true;
     }
 
     public void SetIntensity(float t)
     {
+        Init();
         if (!_initialized) return;
 
         bool active = t > 0.01f;
 
-        if (rainMain)
-        {
-            if (active && !rainMain.isPlaying) rainMain.Play(true);
-            else if (!active && rainMain.isPlaying) rainMain.Stop(true);
-            _emMain.rateOverTime = t * maxEmissionRate;
-        }
+        Drive(rainMain, ref _emMain, active, t * maxEmissionRate);
+        if (rainFine) Drive(rainFine, ref _emFine, active, t * maxFineEmissionRate);
 
         if (rainSplash) rainSplash.gameObject.SetActive(active);
         if (rainRipple) rainRipple.gameObject.SetActive(active);
+    }
+
+    static void Drive(ParticleSystem ps, ref ParticleSystem.EmissionModule em, bool active, float rate)
+    {
+        if (ps == null) return;
+        if (active && !ps.isPlaying) ps.Play(true);
+        else if (!active && ps.isPlaying) ps.Stop(true);
+        em.rateOverTime = rate;
     }
 }
